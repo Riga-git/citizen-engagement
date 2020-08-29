7eb4b84684c5c48813a9a6dd056d5519d636bf4b
LRI <luca.rigazzi@gmail.com>
2020-08-29 11:00:53 +0200

    Guard admin

---
+++ src/app/security/guards/admin.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from "../auth.service";
+import { User } from 'src/app/models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+  constructor(private auth: AuthService){}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> {
+      return (
+        this.auth
+          // Use this to check if the user is authenticated
+          .getUser()
+          // If they're authenticated, return true, otherwise, returns an UrlTree to redirect to the login page
+          .pipe(map((user) => (user. ? true : this.router.parseUrl("/login"))))
+      );
+    }
+  }
